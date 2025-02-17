b07eb02e09d2efea007a7aaccc3cdc498352cfc1
Jeffin <jmathew330@gmail.com>
2025-02-17 13:10:03 -0500

    Implement weather API

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p className="footer__text">Developed by Jeffin Mathew</p>
+      <p className="footer__text">2025</p>
+    </div>
+  );
+}
+
+export default Footer;
